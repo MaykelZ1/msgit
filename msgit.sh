14ecfc918afa12100001899a9618395e56ec7efd
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 15:32:07 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -1,16 +1,46 @@
-warning: in the working copy of 'mensajes.md', LF will be replaced by CRLF the next time Git touches it
-[main a6b747c] Agregado mensaje
- 1 file changed, 1 insertion(+)
-fatal: credential-cache unavailable; no unix socket support
-Enumerating objects: 5, done.
-Counting objects: 100% (5/5), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 368 bytes | 368.00 KiB/s, done.
-Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
-To https://github.com/MaykelZ1/msgit.git
-   3dbd1b2..a6b747c  main -> main
-branch 'main' set up to track 'origin/main'.
-From https://github.com/MaykelZ1/msgit
- * branch            main       -> FETCH_HEAD
-Already up to date.
+#!/bin/bash
+
+# Configuración
+usuario="MaykelZ1"
+token="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"
+repositorio="msgit"
+archivo="mensajes.md"
+
+# Clonar el archivo mensajes.md desde el repositorio
+git clone https://github.com/$usuario/$repositorio.git
+cd $repositorio
+
+while true
+do
+    # Mostrar los últimos 5 mensajes
+    tail -n5 mensajes.md &
+
+    # Esperar 5 segundos
+    sleep 5
+
+    # Limpiar la pantalla
+    echo ""
+
+    # Esperar a que el usuario ingrese un mensaje
+    read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': " mensaje
+
+    # Si se ingresa 'exit', salir del loop
+    if [ "$mensaje" == "exit" ]; then
+        echo "Saliendo..."
+        exit
+    fi
+
+    # Agregar el mensaje al archivo mensajes.md
+    echo "$mensaje" >> $archivo
+
+    # Esperar 3 segundos
+    sleep 3
+
+    # Hacer commit y push del archivo mensajes.md
+    git add $archivo
+    git commit -m "Agregado mensaje"
+    git push -u origin main
+
+    # Actualizar el archivo mensajes.md
+    git pull origin main
+done
