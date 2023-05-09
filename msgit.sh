5261caeedf98776c3196d52ee0684e919f9a02db
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 14:34:15 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+# Configuración
 usuario="MaykelZ1"
 token="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"
 repositorio="msgit"
 archivo="mensajes.md"
 
-echo "Mensajes existentes:"
-curl -H "Authorization: token $token" -s "https://api.github.com/repos/$usuario/$repositorio/contents/$archivo" | grep -oP '(?<="download_url": ")[^"]*' | xargs curl -s | base64 --decode
+# Clonar el archivo mensajes.md desde el repositorio
+git clone https://github.com/$usuario/$repositorio.git
+cd $repositorio
 
-while true; do
-    read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe \"exit\": " mensaje
+while true
+do
+    # Esperar a que el usuario ingrese un mensaje
+    read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': " mensaje
 
+    # Si se ingresa 'exit', salir del loop
     if [ "$mensaje" == "exit" ]; then
-        echo "Adios."
-        break
+        echo "Saliendo..."
+        exit
     fi
 
+    # Agregar el mensaje al archivo mensajes.md
     echo "$mensaje" >> $archivo
-    git add $archivo
-    git commit -m "{\"message\":\"Agregando mensaje: $mensaje\",\"content\":\"$(base64 $archivo)\",\"branch\":\"main\"}" >/dev/null 2>&1
-    git push --set-upstream origin main >/dev/null 2>&1
 
-    echo "Mensaje guardado y subido al repositorio en GitHub."
+    # Esperar 3 segundos
+    sleep 3
+
+    # Hacer commit y push del archivo mensajes.md
+    git add $archivo
+    git commit -m "Agregado mensaje"
+    git push -u origin main
+
+    # Actualizar el archivo mensajes.md
+    git pull origin main
 done
