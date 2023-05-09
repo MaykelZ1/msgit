0a3770d763674d01ef18d07aa57d0df0259dcb36
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 14:39:11 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-# Configuración
 usuario="MaykelZ1"
 token="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"
 repositorio="msgit"
 archivo="mensajes.md"
 
-# Clonar el archivo mensajes.md desde el repositorio
-git clone https://github.com/$usuario/$repositorio.git
-cd $repositorio
+# Clonar el repositorio si no existe
+if [ ! -d "msgit" ]; then
+  git clone "https://github.com/$usuario/$repositorio.git" msgit
+fi
 
 while true
 do
-    # Esperar a que el usuario ingrese un mensaje
-    read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': " mensaje
+  echo "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': "
+  read mensaje
 
-    # Si se ingresa 'exit', salir del loop
-    if [ "$mensaje" == "exit" ]; then
-        echo "Saliendo..."
-        exit
-    fi
+  if [ "$mensaje" == "exit" ]; then
+    break
+  else
+    echo "$mensaje" >> msgit/$archivo
+    echo "Mensaje guardado en $archivo."
+  fi
 
-    # Agregar el mensaje al archivo mensajes.md
-    echo "$mensaje" >> $archivo
+  # Esperar 3 segundos antes de hacer el commit y push
+  sleep 3
 
-    # Esperar 3 segundos
-    sleep 3
+  # Hacer commit y push del archivo
+  cd msgit
+  git add $archivo
+  git commit -m "Actualización de mensajes"
+  git push origin main
+  cd ..
 
-    # Hacer commit y push del archivo mensajes.md
-    git add $archivo
-    git commit -m "Agregado mensaje"
-    git push -u origin main
+  echo "Mensaje subido al repositorio en GitHub."
 
-    # Actualizar el archivo mensajes.md
-    git pull origin main
+  # Descargar la versión más reciente del archivo mensajes.md
+  curl -s "https://raw.githubusercontent.com/$usuario/$repositorio/main/$archivo" | jq -sR '.' > $archivo
+
+  # Mostrar el contenido del archivo mensajes.md
+  if [ -s "$archivo" ]; then
+    echo "Mensajes existentes:"
+    cat $archivo
+  else
+    echo "No hay mensajes nuevos."
+  fi
 done
