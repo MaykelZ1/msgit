55bbad7bc846a1b2257f611ba47f4b73d76a30a3
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 15:07:14 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -6,30 +6,21 @@ token="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"
 repositorio="msgit"
 archivo="mensajes.md"
 
-# Función para clonar el archivo mensajes.md desde el repositorio
-clonar_archivo() {
-    echo "Clonando archivo mensajes.md desde el repositorio..."
-    git clone https://github.com/$usuario/$repositorio.git > /dev/null 2>&1
-    cd $repositorio
-}
-
-# Función para mostrar los últimos 5 mensajes del archivo mensajes.md
-mostrar_ultimos_mensajes() {
-    echo "Últimos mensajes:"
-    tail -n 5 $archivo
-}
-
 # Clonar el archivo mensajes.md desde el repositorio
-clonar_archivo
-# Mostrar los últimos 5 mensajes del archivo mensajes.md
-mostrar_ultimos_mensajes
+echo "Clonando archivo mensajes.md desde el repositorio..."
+git clone -q https://github.com/$usuario/$repositorio.git
+cd $repositorio
 
-# Loop infinito para agregar mensajes al archivo mensajes.md y subirlos al repositorio
 while true
 do
-    # Esperar 5 segundos antes de clonar el archivo mensajes.md
+    # Clonar el archivo mensajes.md cada 5 segundos
     sleep 5
-    clonar_archivo
+    git pull -q
+
+    # Limpiar la pantalla y mostrar los últimos 5 renglones del archivo mensajes.md
+    clear
+    echo "Últimos 5 mensajes:"
+    tail -n 5 $archivo
 
     # Esperar a que el usuario ingrese un mensaje
     read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': " mensaje
@@ -43,16 +34,14 @@ do
     # Agregar el mensaje al archivo mensajes.md
     echo "$mensaje" >> $archivo
 
+    # Esperar 3 segundos
+    sleep 3
+
     # Hacer commit y push del archivo mensajes.md
-    echo "Subiendo mensaje al repositorio..."
-    git add $archivo > /dev/null 2>&1
-    git commit -m "Agregado mensaje" > /dev/null 2>&1
-    git push -u origin main > /dev/null 2>&1
+    git add $archivo
+    git commit -m "Agregado mensaje"
+    git push -q -u origin main
 
     # Actualizar el archivo mensajes.md
-    echo "Actualizando archivo mensajes.md desde el repositorio..."
-    git pull origin main > /dev/null 2>&1
-
-    # Mostrar los últimos 5 mensajes del archivo mensajes.md
-    mostrar_ultimos_mensajes
+    git pull -q
 done
