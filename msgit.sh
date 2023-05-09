7fa97b7aff557ddfd0c59bac79c6530d4502a2af
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 13:33:55 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # Nombre de usuario y nombre del repositorio en GitHub
-usuario="tu_usuario"
-repositorio="tu_repositorio"
+usuario="MaykelZ1"
+repositorio="msgit"
 
 # Ruta del archivo que se creará con los mensajes
 archivo="mensajes.md"
 
-# URL para subir el archivo al repositorio en GitHub
-url="https://api.github.com/repos/MaykelZ1/msgit/contents/msgit.md"
+# Función para subir los mensajes al repositorio en GitHub
+upload_messages_to_github() {
+  mensaje_commit="Agregando mensajes"
+  contenido=$(base64 $archivo)
+  json="{\"message\":\"$mensaje_commit\",\"content\":\"$contenido\"}"
+  respuesta=$(curl -s -H "Authorization: token $token" -H "Content-Type: application/json" -X PUT -d "$json" "https://api.github.com/repos/$usuario/$repositorio/contents/$archivo?ref=main")
+  echo "Mensajes subidos al repositorio en GitHub"
+}
 
 # Token de acceso personal de GitHub
 token="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"
@@ -20,18 +26,8 @@ while true; do
     break
   else
     echo "$mensaje" >> $archivo
+    upload_messages_to_github
   fi
 done
 
-echo "Subiendo mensajes al repositorio en GitHub..."
-
-# Codificar el contenido del archivo en Base64
-contenido=$(base64 $archivo)
-
-# Crear el JSON para la solicitud POST
-json=$(printf '{"message":"Agregando mensajes","content":"%s"}' "$contenido")
-
-# Enviar la solicitud POST para subir el archivo al repositorio
-curl --header "Authorization: token $token" --header "Content-Type: application/json" --data "$json" $url
-
-echo "Mensajes subidos al repositorio en GitHub"
+echo "Fin del programa"
