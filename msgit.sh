9df0c22536cd3f6e91037fcd5d7078255dda8fac
MaykelZ1 <112445894+MaykelZ1@users.noreply.github.com>
2023-05-09 16:26:44 -0300

    Update msgit.sh

--- msgit.sh
+++ msgit.sh
@@ -6,23 +6,40 @@ token="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"
 repositorio="msgit"
 archivo="mensajes.md"
 
-# Clonar el archivo mensajes.md desde el repositorio
-git clone https://github.com/$usuario/$repositorio.git
-cd $repositorio
-
 while true
 do
+    # Comprobar si se ha presionado la tecla F1
+    if [[ $- == *i* ]] && [[ -t 0 ]]; then
+        read -s -n1 key
+        if [ "$key" == $'\x1b' ]; then
+            read -s -n2 key
+            if [ "$key" == "[[A" ]; then
+                echo -e "\nReiniciando el script..."
+                sleep 1
+                clear
+                exec $0
+            fi
+        fi
+    fi
+
+    # Clonar el archivo mensajes.md desde el repositorio
+    if [ ! -d "$repositorio" ]; then
+        echo -ne "\e[36mClonando archivo mensajes.md desde el repositorio...\n\e[0m"
+        git clone https://github.com/$usuario/$repositorio.git > /dev/null 2>&1
+    fi
+    cd $repositorio
+
     # Mostrar los últimos 5 mensajes
-    tail -n5 mensajes.md &
+    echo -ne "\e[36mComprobando mensajes nuevos...\n\e[0m"
+    git pull origin main > /dev/null 2>&1
+    echo -e "\e[36mMostrando los últimos 5 mensajes:\e[0m"
+    tail -n5 mensajes.md
 
     # Esperar 5 segundos
     sleep 5
 
-    # Limpiar la pantalla
-    echo ""
-
     # Esperar a que el usuario ingrese un mensaje
-    read -p "Escribe tu mensaje y presiona Enter. Para terminar, escribe 'exit': " mensaje
+    read -p $'\e[38;5;208mEscribe algo: \e[0m' mensaje
 
     # Si se ingresa 'exit', salir del loop
     if [ "$mensaje" == "exit" ]; then
@@ -33,14 +50,12 @@ do
     # Agregar el mensaje al archivo mensajes.md
     echo "$mensaje" >> $archivo
 
-    # Esperar 3 segundos
-    sleep 3
+    # Esperar 1 segundo
+    sleep 2
 
     # Hacer commit y push del archivo mensajes.md
-    git add $archivo
-    git commit -m "Agregado mensaje"
-    git push -u origin main
+    git add $archivo > /dev/null 2>&1
+    git commit -m "Agregado mensaje" > /dev/null 2>&1
+    git push -u origin main > /dev/null 2>&1
 
-    # Actualizar el archivo mensajes.md
-    git pull origin main
 done
